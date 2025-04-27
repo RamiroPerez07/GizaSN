80e769276a4914224cfda34b43971fa1e28d46b8
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-04-27 02:33:17 -0300

    agregado de imagenes banners

--- src/app/components/hero-carousel/hero-carousel.component.ts
+++ src/app/components/hero-carousel/hero-carousel.component.ts
@@ -10,11 +10,9 @@ import { Component } from '@angular/core';
 })
 export class HeroCarouselComponent {
   items = [
-    { image: 'https://images.pexels.com/photos/4181706/pexels-photo-4181706.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', caption: 'Running for fitness' },
-    { image: 'https://images.pexels.com/photos/798768/pexels-photo-798768.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', caption: 'Supplements for strength' },
-    { image: 'https://images.pexels.com/photos/414029/pexels-photo-414029.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', caption: 'Training hard in the gym' },
-    { image: 'https://images.pexels.com/photos/6001390/pexels-photo-6001390.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', caption: 'Nutrition and supplements' },
-    { image: 'https://images.pexels.com/photos/4193612/pexels-photo-4193612.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500', caption: 'Gym workouts for success' }
+    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728518/WhatsApp_Image_2025-04-26_at_21.31.11_sgnyqc.jpg', caption: 'Running for fitness' },
+    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728518/WhatsApp_Image_2025-04-26_at_22.47.48_etffl4.jpg', caption: 'Supplements for strength' },
+    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728519/WhatsApp_Image_2025-04-26_at_21.52.55_nedulp.jpg', caption: 'Training hard in the gym' },
   ];
 
   currentIndex = 0;
