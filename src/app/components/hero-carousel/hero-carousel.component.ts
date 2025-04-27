217d1c0884f5c0ba66063fe210e461e126c4bc6b
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-04-27 02:45:01 -0300

    agregado de rutas

--- src/app/components/hero-carousel/hero-carousel.component.ts
+++ src/app/components/hero-carousel/hero-carousel.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-hero-carousel',
@@ -9,11 +11,31 @@ import { Component } from '@angular/core';
   styleUrl: './hero-carousel.component.css'
 })
 export class HeroCarouselComponent {
+
+  readonly routerSvc = inject(Router);
+
+  readonly productsSvc = inject(ProductsService);
+
   items = [
-    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728518/WhatsApp_Image_2025-04-26_at_21.31.11_sgnyqc.jpg', caption: 'Running for fitness' },
-    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728518/WhatsApp_Image_2025-04-26_at_22.47.48_etffl4.jpg', caption: 'Supplements for strength' },
-    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728519/WhatsApp_Image_2025-04-26_at_21.52.55_nedulp.jpg', caption: 'Training hard in the gym' },
+    { 
+      image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728518/WhatsApp_Image_2025-04-26_at_21.31.11_sgnyqc.jpg', 
+      caption: 'Running for fitness', 
+      redirectTo: () => {
+        this.routerSvc.navigate(["products"])
+        this.productsSvc.filterProductsByCategory({
+          id: "1.2",
+          txt_category: "Nutremax",
+          priority: 20,
+          level: 2,
+          children: null,
+      });
+      } 
+    },
+    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728518/WhatsApp_Image_2025-04-26_at_22.47.48_etffl4.jpg', caption: 'Supplements for strength', redirectTo: () => {} },
+    { image: 'https://res.cloudinary.com/dhnicvwkw/image/upload/v1745728519/WhatsApp_Image_2025-04-26_at_21.52.55_nedulp.jpg', caption: 'Training hard in the gym', redirectTo: () => {} },
   ];
+
+  
 
   currentIndex = 0;
   transformValue = 0;
