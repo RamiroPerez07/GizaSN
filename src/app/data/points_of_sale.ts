a35741513e3b088dd5f9cee9c35e8c21d3361597
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-07-04 18:33:46 -0300

    corregido numero

--- src/app/data/points_of_sale.ts
+++ src/app/data/points_of_sale.ts
@@ -4,25 +4,25 @@ export const POINTS_OF_SALE : PointOfSale[] = [
   {
     id:"ironman",
     puntoDeVenta: "Ironman Gym",
-    telefono: "3364401635",
+    telefono: "3364358745",
     contacto: "Pablo Brunelli"
   },
   {
     id:"atenas",
     puntoDeVenta: "Atenas Gym",
-    telefono: "3364401635",
+    telefono: "3364358745",
     contacto: "Esteban"
   },
   {
     id:"giza",
     puntoDeVenta: "GIZA",
-    telefono: "3364401635",
+    telefono: "3364358745",
     contacto: "Clari"
   },
   {
     id:"eduardo-vinas",
     puntoDeVenta: "Eduardo Viñas",
-    telefono: "3364401635",
+    telefono: "3364358745",
     contacto: "Eduardo Viñas"
   },
 ]
