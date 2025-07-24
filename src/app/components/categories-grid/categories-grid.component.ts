e71ee2ebd3d2054204117d443abb29c46ad9dbb5
RamiroPerez07 <ramiperez1997@hotmail.com>
2025-07-24 15:19:54 -0300

    change of mind

--- src/app/components/categories-grid/categories-grid.component.ts
+++ src/app/components/categories-grid/categories-grid.component.ts
@@ -1,8 +1,7 @@
-import { Component, inject, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, inject, Input} from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { ICategory } from '../../interfaces/categories.interface';
 import { Router } from '@angular/router';
-import { combineLatest, Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { PointOfSaleService } from '../../services/point-of-sale.service';
 
@@ -13,9 +12,9 @@ import { PointOfSaleService } from '../../services/point-of-sale.service';
   templateUrl: './categories-grid.component.html',
   styleUrl: './categories-grid.component.css'
 })
-export class CategoriesGridComponent implements OnInit, OnDestroy {
+export class CategoriesGridComponent {
 
-  @Input() parentCategoryId!: string;
+  @Input() categories!: ICategory[];
   
   @Input() title! : string;
 
@@ -24,29 +23,6 @@ export class CategoriesGridComponent implements OnInit, OnDestroy {
   readonly router = inject(Router);
 
   readonly posSvc = inject(PointOfSaleService);
-
-  categories! : ICategory[];
-
-  private sub!: Subscription;
-
-  ngOnInit(): void {
-    // Observa cambios tanto en la lista de categorías como en los allowedCategoryIds
-    this.sub = combineLatest([
-      this.productsSvc.$categories,
-      this.productsSvc.$allowedCategoryIds,
-      this.posSvc.$pos
-    ]).subscribe(([categories, allowedIds, pos]) => {
-      this.updateCategories();
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.sub?.unsubscribe();
-  }
-
-  updateCategories() {
-    this.categories = this.productsSvc.getCategoriesByParent(this.parentCategoryId);
-  }
 
   redirectTo(category: ICategory){
     this.router.navigate(['products', 'category', category.id]);
--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { HeroCarouselComponent } from "../hero-carousel/hero-carousel.component";
 import { BenefitsComponent } from "../benefits/benefits.component";
 import { CardCarouselComponent } from "../card-carousel/card-carousel.component";
@@ -7,6 +7,8 @@ import { Router } from '@angular/router';
 import { CategoriesGridComponent } from "../categories-grid/categories-grid.component";
 import { ProductsService } from '../../services/products.service';
 import { IProduct } from '../../interfaces/products.interface';
+import { Subscription } from 'rxjs';
+import { ICategory } from '../../interfaces/categories.interface';
 
 @Component({
   selector: 'app-hero',
@@ -15,7 +17,7 @@ import { IProduct } from '../../interfaces/products.interface';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements OnInit, OnDestroy {
 
   readonly routerSvc = inject(Router);
 
@@ -27,8 +29,24 @@ export class HeroComponent implements OnInit {
     this.routerSvc.navigate([path])
   }
 
+  sub! : Subscription;
+
+  nutremaxCategories! : ICategory[];
+  innovaCategories! : ICategory[];
+
   ngOnInit(): void {
-      this.products = this.productsSvc.getProductsForHeroCarousel();
-    }
+    this.products = this.productsSvc.getProductsForHeroCarousel();
+
+    this.sub = this.productsSvc.$categories.subscribe({
+      next: (categories) => {
+        this.nutremaxCategories = this.productsSvc.getCategoriesByParent('nutremax');
+        this.innovaCategories = this.productsSvc.getCategoriesByParent('innovanaturals');
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
 
 }
